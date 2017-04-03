1ca0d1cbf5c8007210932016b02e6cb84f9c6e3a
b3n10 <francisben.lleve@gmail.com>
2017-04-03 15:52:00 +0800

    edit youtube php

--- youtube.php
+++ youtube.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="description" content="Video">
-	<title>Video page</title>
+	<title>Video page - edit</title>
 </head>
 <body>
 <iframe width="560" height="315" src="https://www.youtube.com/embed/fJEZoye7Iys?list=PLfdtiltiRHWHjTPiFDRdTOPtSyYfz3iLW" frameborder="0" allowfullscreen></iframe>
