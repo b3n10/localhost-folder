56b732fb342da04117eea3ed25b0bfa82b081ce8
b3n10 <francisben.lleve@gmail.com>
2017-04-14 10:22:09 +0800

    Updated php files

--- php-basics/day-of-week.php
+++ php-basics/day-of-week.php
@@ -1,38 +1,33 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Week of the day</title>
-    </head>
-    <body>
-        <?php
-        $day_of_week = date('w');
-        echo 'Today is ';
+<?php
+$title = "Day of Week";
+include 'head-tag.php';
 
-        switch ($day_of_week) {
-            case 1:
-                echo "Monday";
-                break;
-            case 2:
-                echo "Tueday";
-                break;
-            case 3:
-                echo "Wednesday";
-                break;
-            case 4:
-                echo "Thursday";
-                break;
-            case 5:
-                echo "Friday";
-                break;
-            case 6:
-                echo "Saturday";
-                break;
-            case 7:
-                echo "Sunday";
-                break;
-        }
-         ?>
+$day_of_week = date('w');
+echo 'Today is ';
 
-    </body>
-</html>
+switch ($day_of_week) {
+    case 1:
+    echo "Monday";
+    break;
+    case 2:
+    echo "Tueday";
+    break;
+    case 3:
+    echo "Wednesday";
+    break;
+    case 4:
+    echo "Thursday";
+    break;
+    case 5:
+    echo "Friday";
+    break;
+    case 6:
+    echo "Saturday";
+    break;
+    case 7:
+    echo "Sunday";
+    break;
+}
+
+include 'end-tag.php';
+?>
